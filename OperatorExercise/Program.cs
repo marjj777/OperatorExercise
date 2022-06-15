f48ba29a78e0664aa9bb9d12bb6f09e542c4c19e
Joel Rosen <joel@velarium.com>
2022-06-15 17:05:00 -0600

    Operators!

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if(a == 17 && b == 4)
+            {
+                Console.WriteLine($"The answer is {quotient} and {remainder}");
+            }
+
+
+        }
+    }
+}
